6986eafc7247712f3dff03e36e4abcac1890a938
uefue <uefue@student.kit.edu>
2024-08-10 20:49:57 +0200

    initial commit (init spring project)

---
+++ src/main/java/io/github/ardoco/rest/ArDoCoRestApplication.java
@@ -0,0 +1,13 @@
+package io.github.ardoco.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArDoCoRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArDoCoRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ardoco/rest/ArDoCoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ardoco.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArDoCoRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
